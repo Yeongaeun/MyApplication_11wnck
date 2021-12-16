48905e036d8836f7b656b3fae775b5c152615a43
82108 <dusrkdms0928@naver.com>
2021-12-16 15:53:44 +0900

    upload

--- app/src/main/java/com/example/myapplication_11/MainActivity.java
+++ app/src/main/java/com/example/myapplication_11/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication_11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,13 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
     static RequestQueue requestQueue;
 
+    RecyclerView recyclerView;
+    MovieAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         editText=findViewById(R.id.xeditText);
-        textView= findViewById(R.id.xtextView);
+        textView= findViewById(R.id.textView);
 
         Button button = findViewById(R.id.xebutton);
         button.setOnClickListener(new View.OnClickListener(){
@@ -45,6 +52,15 @@ public class MainActivity extends AppCompatActivity {
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
 
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager
+                (this,LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new MovieAdapter();
+        recyclerView.setAdapter(adapter);
+
+
     }
     public void makeRequest() {
         String url = editText.getText().toString();
@@ -56,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         println("응답 ->" + response);
+                        processResponse(response);
                     }
                 },
 
@@ -76,8 +93,20 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(request);
         println("요청보냄");
     }
-    public void println(String data){
-        textView.append(data + "\n");
+    public void println(String data) {
+        Log.d("MainActivity", data);
+    }
+    public void processResponse(String response)
+    {
+        Gson gson = new Gson();
+        MovieList movieList = gson.fromJson(response,MovieList.class);
+        println("영화 정보의 수:" + movieList.boxOfficeResult.dailyBoxOfficeList.size());
+
+        for(int i =0; i< movieList.boxOfficeResult.dailyBoxOfficeList.size();i++){
+            Movie movie = movieList.boxOfficeResult.dailyBoxOfficeList.get(i);
+            adapter.addItem(movie);
+        }
+        adapter.notifyDataSetChanged();
     }
 }
 
---
+++ app/src/main/java/com/example/myapplication_11/Movie.java
@@ -0,0 +1,21 @@
+package com.example.myapplication_11;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String selesAmt;
+    String salesShare;
+    String salesInten;
+    String saledsChange;
+    String salesAcc;
+    String adiCnt;
+    String andiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
